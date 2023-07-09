4ecaa9a36e57b291ac98f7d1cc9ffa2469aa4318
raku <lakjinteo@gmail.com>
2023-07-09 18:00:15 +0800

    complete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,7 +6,16 @@ import { usePathname,useRouter } from "next/navigation"
 
 export default function PromptCard({post,handleTagClick,handleEdit,handleDelete}) {
 
+    const {data:session} = useSession()
     const [copied,setCopied] = useState('')
+    const pathName = usePathname()
+    const router = useRouter()
+
+    const handleCopy = () => {
+        setCopied(post.prompt)
+        navigator.clipboard.writeText(post.prompt)
+        setTimeout(()=>setCopied(""),3000)
+    }
 
     return (
         <div className="prompt_card">
@@ -19,7 +28,7 @@ export default function PromptCard({post,handleTagClick,handleEdit,handleDelete}
                     </div>
                 </div>
 
-                <div className="copy_btn" onClick={() => {}}>
+                <div className="copy_btn" onClick={handleCopy}>
                     <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} width={12} height={12} />
                 </div>
             </div>
@@ -30,6 +39,13 @@ export default function PromptCard({post,handleTagClick,handleEdit,handleDelete}
                     {post.tag}
                 </p>
             </div>
+            {session?.user.id === post.creator._id && pathName === '/profile' && 
+            (
+                <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+                    <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>Edit</p>
+                    <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>Delete</p>
+                </div>
+            )}
         </div>
     )
 }

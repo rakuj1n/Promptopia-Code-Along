bb6b3c299a91afe9a7fea1deaefa69bc61e7f2b2
raku <lakjinteo@gmail.com>
2023-07-09 13:04:57 +0800

    1:42:03

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import '@styles/globals.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: "Promptopia",
+    dscription: "Discover & Share AI Prompts"
+}
+
+export default function RootLayout({children}) {
+
+
+    return (
+        <html lang='en'>
+            <body>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
+}
---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery')
+
+    if(isConnected) {
+        console.log('MongoDB is already conencted')
+        return
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName:'share_prompt',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        isConnected = true
+        console.log("MongoDB connected")
+    } catch (err) {
+        console.log(err)
+    }
+
+}
